9559e76e558785f27fad344fb90cd0a2241dfbac
pedro-lucinda <pedrolucinda96@gmail.com>
2025-07-23 14:32:45 -0300

    wip

--- .history/middleware_20250722210144.ts
+++ .history/middleware_20250722210144.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
-import { auth0 } from "./lib/auth0";
+import { auth0 } from "./libs/auth0";
 
 export async function middleware(request: NextRequest) {
   const authRes = await auth0.middleware(request)
